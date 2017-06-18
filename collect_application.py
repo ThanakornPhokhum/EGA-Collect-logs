7269d99258c5bc8741208d4ec6c916858d31beb7
ThanakornPhokhum <bleach.199396@gmail.com>
2017-06-19 03:19:26 +0700

    Add Collect Session
    show session info

--- collect_application.py
+++ collect_application.py
@@ -1,10 +1,24 @@
-import requests
+import requests, os
 from time import gmtime, strftime
+
+
+file_path = (strftime("D:\OneDrive - Ascend Corp\ETC\Log_Palo\%Y-%m-%d_Application", gmtime()))
+
+
+def ensure_dir():
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+        return "false"
+
+    if os.path.exists(file_path):
+        return "ok"
+
+ensure_dir()
 
 url = "https://10.9.40.3/api/?type=report&async=yes&reporttype=predefined&reportname=top-applications&&key=LUFRPT0vTGM1UWp0cUh3cG1vcWxKc1U5dnBTQXc3emc9cVpXcWRXZ3RWOVYvMzcyUnlDOEZYTDU5anNUZXNQellaTVkxV2tveFFTVT0="
 
 response = requests.get(url, verify=False)
-filename = strftime("\%Y-%m-%d_%H-%M-%S_application.xml", gmtime())
-file_handle = open("D:\Log_Palo"+filename,"w")
+filename = strftime("\%Y-%m-%d_application\%Y-%m-%d_%H-%M-%S_application.xml", gmtime())
+file_handle = open("D:\OneDrive - Ascend Corp\ETC\Log_Palo"+filename,"w")
 file_handle.write(response.text)
 file_handle.close()
--- session.py
+++ session.py
@@ -1,10 +1,24 @@
-import requests
+import requests, os
 from time import gmtime, strftime
 
-url = "https://10.9.40.3/api/?type=op&cmd=%3Cshow%3E%3Crunning%3E%3Cresource-monitor%3E%3Cminute%3E%3C%2Fminute%3E%3C%2Fresource-monitor%3E%3C%2Frunning%3E%3C%2Fshow%3E&key=LUFRPT0vTGM1UWp0cUh3cG1vcWxKc1U5dnBTQXc3emc9cVpXcWRXZ3RWOVYvMzcyUnlDOEZYTDU5anNUZXNQellaTVkxV2tveFFTVT0="
+file_path = (strftime("D:\OneDrive - Ascend Corp\ETC\Log_Palo\%Y-%m-%d_Session", gmtime()))
+
+
+def ensure_dir():
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+        return "false"
+
+    if os.path.exists(file_path):
+        return "ok"
+
+ensure_dir()
+
+url = "https://10.9.40.3/api/?type=op&cmd=%3Cshow%3E%3Csession%3E%3Cinfo%3E%3C%2Finfo%3E%3C%2Fsession%3E%3C%2Fshow%3E&key=LUFRPT0vTGM1UWp0cUh3cG1vcWxKc1U5dnBTQXc3emc9cVpXcWRXZ3RWOVYvMzcyUnlDOEZYTDU5anNUZXNQellaTVkxV2tveFFTVT0="
+
 
 response = requests.get(url, verify=False)
-filename = strftime("\%Y-%m-%d_%H-%M-%S_Session.xml", gmtime())
-file_handle = open("C:\Users\DDuser\Desktop\Log_palo"+filename,"w")
+filename = strftime("\%Y-%m-%d_Session\%Y-%m-%d_%H-%M-%S_Session.xml", gmtime())
+file_handle = open("D:\OneDrive - Ascend Corp\ETC\Log_Palo"+filename,"w")
 file_handle.write(response.text)
 file_handle.close()
